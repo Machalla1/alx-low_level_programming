a7dac9c396da4cf695de508f788a4926f041e0b7
Machalla1 <104342220+Machalla1@users.noreply.github.com>
2023-08-05 02:58:52 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int p;
-        unsigned int dec_val = 0;
+	unsigned int num = 0;
 
-        if (!b)
-                return (0);
-
-        for (p = 0; b[p]; p++)
-        {
-                if (b[p] < '0' || b[p] > '1')
-                        return (0);
-                dec_val = 2 * dec_val + (b[p] - '0');
-        }
-
-        return (dec_val);
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		num = (2 * num + (*b - '0'));
+		b++;
+	}
+	return (num);
+}
